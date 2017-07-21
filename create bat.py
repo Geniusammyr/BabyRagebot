2979ecdc7a510717a9fa94d8828afe33a39a9177
Geniusammyr <30309082+Geniusammyr@users.noreply.github.com>
2017-07-21 00:17:59 -0500

    Update create bat.py

--- create bat.py
+++ create bat.py
@@ -2,4 +2,3 @@ import os
 Dir=os.path.split(os.path.abspath(__file__))[0]
 with open(os.path.join(Dir,'Run LuLbot.bat'),'w') as batFile:
 	batFile.write('start cmd.exe /k python '+str(os.path.join(Dir,'LuLbot.py')))
-#start cmd.exe /k python C:\Users\geniusammyr\desktop\LuLbot\personalbot.py
